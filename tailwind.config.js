5216755d42372f501599a43fcb74e2f8b5ffd015
Smit Sojitra <smitsojitra810@gmail.com>
2024-01-04 18:48:32 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  fontFamily:{
+    Whi:["Whitney"],
+    Gi:["Ginto"],
+    gg:["ggSans"],
+  },
+  fontSize: {
+    clamp: "clamp(24px,5vw,44px)",
+  },
+}
